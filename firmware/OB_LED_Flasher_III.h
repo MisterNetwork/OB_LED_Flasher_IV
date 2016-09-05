5ea9abfbe2edfa3c63e7175d89fb0c3fbe83b769
MisterNetwork <MisterNetwork@users.noreply.github.com>
2016-09-05 01:08:45 -0400

    Update OB_LED_Flasher_III.h

--- firmware/OB_LED_Flasher_III.h
+++ firmware/OB_LED_Flasher_III.h
@@ -16,29 +16,21 @@
 *
 *  The following are available:
 *
-*  LED_Red_Flash(count);  // red flash x(count)
-*  LED_Red_FFlash(count);  // red fast flash x(count)
+*  LED_Red_Flash(count,onRate,offRate);  // red flash x(count)
 *  
-*  LED_Green_Flash(count); // green flash x(count)
-*  LED_Green_FFlash(count); // green fast flash x(count)
+*  LED_Green_Flash(count,onRate,offRate); // green flash x(count)
 *  
-*  LED_Blue_Flash(count); // blue flash x(count)
-*  LED_Blue_FFlash(count); // blue fast flash x(count)
+*  LED_Blue_Flash(count,onRate,offRate); // blue flash x(count)
 *  
-*  LED_Yellow_Flash(count); // yellow flash x(count)
-*  LED_Yellow_FFlash(count); // yellow fast flash x(count)
+*  LED_Yellow_Flash(count,onRate,offRate); // yellow flash x(count)
 *  
-*  LED_Orange_Flash(count); // orange flash x(count)
-*  LED_Orange_FFlash(count); // orange fast flash x(count)
+*  LED_Orange_Flash(count,onRate,offRate); // orange flash x(count)
 *  
-*  LED_Cyan_Flash(count); // cyan flash x(count)
-*  LED_Cyan_FFlash(count); // cyan fast flash x(count)
+*  LED_Cyan_Flash(count,onRate,offRate); // cyan flash x(count)
 *  
-*  LED_Magenta_Flash(count); // magenta flash x(count)
-*  LED_Magenta_FFlash(count); // magenta fast flash x(count)
+*  LED_Magenta_Flash(count,onRate,offRate); // magenta flash x(count)
 *  
-*  LED_White_Flash(count); // white flash x(count)
-*  LED_White_FFlash(count); // white fast flash x(count)
+*  LED_White_Flash(count,onRate,offRate); // white flash x(count)
 *  
 *  LED_Rainbow_Flash(count); // RGB flash x(count)
 *  
